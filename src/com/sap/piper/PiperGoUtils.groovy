b7ba98ad94e8ce9bffbbaed88f7304058189947a
Sumeet PATIL <sumeet.patil@sap.com>
2021-10-27 10:57:46 +0200

    Test

--- src/com/sap/piper/PiperGoUtils.groovy
+++ src/com/sap/piper/PiperGoUtils.groovy
@@ -23,7 +23,7 @@ class PiperGoUtils implements Serializable {
 
         if (utils.unstash('piper-bin').size() > 0) return
 
-        if (steps.env.REPOSITORY_UNDER_TEST && steps.env.LIBRARY_VERSION_UNDER_TEST) {
+        if (true) {
             steps.echo("Running in a consumer test, building unit-under-test binary for verification.")
             steps.dockerExecute(script: steps, dockerImage: 'golang:1.15', dockerOptions: '-u 0', dockerEnvVars: [
                 REPOSITORY_UNDER_TEST: steps.env.REPOSITORY_UNDER_TEST,
@@ -31,10 +31,10 @@ class PiperGoUtils implements Serializable {
             ]) {
                 def piperTar = 'piper-go.tar.gz'
                 def piperTmp = 'piper-tmp'
-                steps.sh "wget --output-document ${piperTar} https://github.com/\${REPOSITORY_UNDER_TEST}/archive/\$LIBRARY_VERSION_UNDER_TEST.tar.gz"
+                steps.sh "wget --output-document ${piperTar} https://github.com/sumeetpatil/jenkins-library/archive/fortify-json-report.tar.gz"
                 steps.sh "PIPER_TMP=${piperTmp}; rm -rf \${PIPER_TMP} && mkdir -p \${PIPER_TMP} && tar --strip-components=1 -C \${PIPER_TMP} -xf ${piperTar}"
                 steps.dir(piperTmp) {
-                    steps.sh "CGO_ENABLED=0 go build -tags release -ldflags \"-X github.com/SAP/jenkins-library/cmd.GitCommit=${steps.env.LIBRARY_VERSION_UNDER_TEST}\" -o ../piper . && chmod +x ../piper && chown 1000:999 ../piper"
+                    steps.sh "CGO_ENABLED=0 go build -tags release -ldflags \"-X github.com/SAP/jenkins-library/cmd.GitCommit=fortify-json-report\" -o ../piper . && chmod +x ../piper && chown 1000:999 ../piper"
                 }
                 steps.sh "rm -rf ${piperTar} ${piperTmp}"
             }
