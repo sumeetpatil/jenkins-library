5ba3e8512ae3ab24d8a4335c2793b904a76a3e33
Sumeet PATIL <sumeet.patil@sap.com>
2022-07-04 16:25:20 +0200

    test

--- vars/codeqlExecuteScan.groovy
+++ vars/codeqlExecuteScan.groovy
@@ -6,10 +6,7 @@ import static com.sap.piper.Prerequisites.checkScript
 
 void call(Map parameters = [:]) {
     final script = checkScript(this, parameters) ?: this
-    for ( e in script.commonPipelineEnvironment ) {
-            print "key = ${e.key}, value = ${e.value}"
-    }
-    print "token = ${env.PIPER_githubToken}"
+    print "commitId = ${script.commonPipelineEnvironment..gitCommitId}"
 
     List credentials = [[type: 'token', id: 'githubTokenCredentialsId', env: ['PIPER_githubToken']]]
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
