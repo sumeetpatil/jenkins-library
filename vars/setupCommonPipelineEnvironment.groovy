0cabea06516da9b2c94c60354aca6122ef491748
Sumeet PATIL <sumeet.patil@sap.com>
2022-08-30 11:25:24 +0200

    test

--- vars/setupCommonPipelineEnvironment.groovy
+++ vars/setupCommonPipelineEnvironment.groovy
@@ -125,7 +125,7 @@ void call(Map parameters = [:]) {
             setGitRefOnCommonPipelineEnvironment(script, scmInfo.GIT_COMMIT, scmInfo.GIT_BRANCH, gitUtils)
         }
 
-        echo commonPipelineEnvironment
+        echo script.commonPipelineEnvironment
     }
 }
 
